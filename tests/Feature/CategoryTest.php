5fa9cd75373f57e0399cf0f22d5ae3e28b42da5a
Misbahudin <misbahudin.id@gmail.com>
2023-08-11 20:29:34 +0700

    Delete Manny

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -126,5 +126,28 @@ class CategoryTest extends TestCase
 
         self::assertEquals(0, $total);
     }
+
+    public function testDeleteMany() {
+        $categories = [];
+
+        for ($i = 0; $i < 10; $i++) {
+            $categories[] = [
+                "id" => "id-$i",
+                "name" => "name-$i"
+            ];
+        }
+
+        $result = Category::query()->insert($categories);
+        self::assertTrue($result);
+
+        $total = Category::query()->count();
+        self::assertEquals(10, $total);
+
+        Category::query()->whereNull("description")->delete();
+
+        $total = Category::query()->count();
+
+        self::assertEquals(0, $total);
+    }
 }
 
