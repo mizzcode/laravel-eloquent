1e07c6ccb590b7dd93b5b8e41a28c2e94b2c7664
Misbahudin <misbahudin.id@gmail.com>
2023-08-20 10:58:54 +0700

    edit: column is_active to default true

--- database/migrations/2023_08_19_103317_add_is_active_column_to_categories_table.php
+++ database/migrations/2023_08_19_103317_add_is_active_column_to_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->boolean("is_active")->nullable()->default(false);
+            $table->boolean("is_active")->nullable()->default(true);
         });
     }
 
